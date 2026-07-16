36e0d87bcd37df2fb0daebcf75b128ae483470ee
Charmando000 <elescogido0310@gmail.com>
2026-07-16 13:45:32 -0300

    Set up app layout and frontend navigation

---
+++ app/(app)/layout..tsx
@@ -0,0 +1,22 @@
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+
+export default function AppLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      <Sidebar />
+
+      <div className="flex flex-1 flex-col">
+        <Navbar />
+
+        <main className="flex-1 p-6">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
